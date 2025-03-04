f7e61c058fe7d4b1cd91a4d9f755daf857bceb4c
Cloudy0219 <2953349195@qq.com>
2025-03-03 22:48:46 -0500

    Add shortest path finder interface

---
+++ src/ShortestPathFinder1.java
@@ -0,0 +1,37 @@
+/**
+ * Enhanced interface for ShortestPathFinder.
+ *
+ * <p>
+ * Extends {@code ShortestPathFinderKernel} by adding higher-level functionality
+ * related to pathfinding.
+ * </p>
+ */
+public interface ShortestPathFinder1 extends ShortestPathFinderKernel {
+
+    /**
+     * Computes the length (cost) of the shortest path between two locations.
+     *
+     * <p>
+     * Depending on the chosen pathfinding algorithm (e.g., Dijkstra's
+     * algorithm, A*), if no path exists between the two locations, you may
+     * return {@code Double.POSITIVE_INFINITY}, -1, or throw an exception.
+     * </p>
+     *
+     * @param from
+     *            the starting location
+     * @param to
+     *            the target location
+     * @return the total cost of the shortest path from {@code from} to
+     *         {@code to}
+     * @requires <pre>
+     * from and to must already exist in this graph
+     * </pre>
+     * @ensures <pre>
+     * shortestPath(from, to) = the minimal total weight of any path
+     *                          from {@code from} to {@code to} in the graph;
+     * if no path exists, the method returns an implementation-specific value.
+     * </pre>
+     */
+    double shortestPath(String from, String to);
+
+}
